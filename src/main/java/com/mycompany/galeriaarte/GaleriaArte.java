60694b89440c5da4168b9e8f61111168a884e94d
IngArci <sarciniegasjimenez@gmail.com>
2025-08-22 23:15:03 -0500

    Primer commit, creación del proyecto

---
+++ src/main/java/com/mycompany/galeriaarte/GaleriaArte.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.galeriaarte;
+
+/**
+ *
+ * @author SANTIAGO
+ */
+public class GaleriaArte {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
